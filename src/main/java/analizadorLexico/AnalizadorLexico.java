aa58be606c9d5ae3d53c1baba4b8f80c6c81aa3c
nomico <nomicomateou@gmail.com>
2024-09-23 12:36:03 -0300

    initial commit

---
+++ src/main/java/analizadorLexico/AnalizadorLexico.java
@@ -0,0 +1,9 @@
+package analizadorLexico;
+import vista.VentanaPrincipal;
+
+public class AnalizadorLexico {
+
+    public static void main(String[] args) {
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+    }
+}
