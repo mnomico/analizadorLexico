43c6e8966724784003460fad2c19637112e1c2c0
nomico <nomicomateou@gmail.com>
2024-10-01 21:01:37 -0300

    add status bar to show line and column of current position

--- src/main/java/vista/VentanaPrincipal.java
+++ src/main/java/vista/VentanaPrincipal.java
@@ -10,10 +10,12 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
+import javax.swing.*;
+import javax.swing.event.CaretEvent;
+import javax.swing.event.CaretListener;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
 
 public class VentanaPrincipal extends javax.swing.JFrame {
 
@@ -29,6 +31,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         setTitle("Nuevo archivo - Analizador Léxico");
         setVisible(true);
+        lnCol.setText("Ln: " + 1 + " Col: " + 1);
         
         addWindowListener(new WindowAdapter(){
             @Override
@@ -43,6 +46,13 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                     }
                 }
             }
+        });
+        
+        panelTexto.addCaretListener((CaretEvent e) -> {
+            int posicion = panelTexto.getCaretPosition();
+            int linea = panelTexto.getDocument().getDefaultRootElement().getElementIndex(posicion) + 1;
+            int columna = posicion - panelTexto.getDocument().getDefaultRootElement().getElement(linea - 1).getStartOffset() + 1;
+            lnCol.setText("Ln: " + linea + " Col: " + columna);
         });
         
         panelTexto.getDocument().addDocumentListener(new DocumentListener() {
@@ -65,6 +75,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                 setTitle("*" + tituloVentana);
             }
         });
+        
     }
 
     /**
@@ -78,6 +89,8 @@ public class VentanaPrincipal extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         panelTexto = new javax.swing.JEditorPane();
+        jToolBar1 = new javax.swing.JToolBar();
+        lnCol = new javax.swing.JLabel();
         barraMenu = new javax.swing.JMenuBar();
         botonArchivo = new javax.swing.JMenu();
         bArchivoNuevo = new javax.swing.JMenuItem();
@@ -91,6 +104,13 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jScrollPane1.setViewportView(panelTexto);
+
+        jToolBar1.setRollover(true);
+        jToolBar1.setFocusable(false);
+
+        lnCol.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lnCol.setText("Ln  Col  ");
+        jToolBar1.add(lnCol);
 
         botonArchivo.setText("Archivo");
         botonArchivo.addActionListener(new java.awt.event.ActionListener() {
@@ -161,15 +181,115 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
+            .addComponent(jToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 434, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 253, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 448, Short.MAX_VALUE)
+                .addGap(2, 2, 2)
+                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void botonArchivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonArchivoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_botonArchivoActionPerformed
+
+    private void bArchivoSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bArchivoSalirActionPerformed
+        int cerrar = cerrarPrograma();
+        if (cerrar == 1){
+            dispose();
+        }
+    }//GEN-LAST:event_bArchivoSalirActionPerformed
+
+    private void bAnalizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bAnalizarActionPerformed
+        if (archivoActual.getSelectedFile() == null){
+            int guardado = guardarArchivoComo();
+            if (guardado == 1){
+                return;
+            }
+        }
+        try {
+            FileReader lectorArchivo = new FileReader(archivoActual.getSelectedFile().getPath());
+            Lexico lexer = new Lexico(lectorArchivo);
+            lexer.next_token();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(VentanaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(VentanaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_bAnalizarActionPerformed
+
+    private void bArchivoGuardarComoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bArchivoGuardarComoActionPerformed
+        guardarArchivoComo();
+    }//GEN-LAST:event_bArchivoGuardarComoActionPerformed
+
+    private void bArchivoGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bArchivoGuardarActionPerformed
+        if (archivoActual.getSelectedFile() == null){
+            guardarArchivoComo();
+        } else {
+            PrintWriter buffer;
+            try {
+                buffer = new PrintWriter(archivoActual.getSelectedFile());
+                buffer.print(panelTexto.getText());
+                buffer.close();
+                tituloVentana = archivoActual.getSelectedFile().getName() + " - Analizador Léxico";
+                setTitle(tituloVentana);
+                modificado = false;
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(VentanaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }//GEN-LAST:event_bArchivoGuardarActionPerformed
+
+    private void bArchivoAbrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bArchivoAbrirActionPerformed
+        if (modificado){
+            int opcion = popupCambios();
+            switch (opcion){
+                case 0 -> {
+                    // Abrir ventana para elegir donde guardar el archivo y abrir el archivo
+                    int i = guardarArchivoComo();
+                    if (i == 0) {
+                        // Si no se cancela el guardado del archivo, se abre el archivo deseado
+                        abrirArchivo();
+                    }
+                }
+                case 1 -> {
+                    // Abre el archivo sin guardar lo anterior
+                    abrirArchivo();
+                }
+                case 2, -1 -> {
+                    // No hacer nada
+                }
+            }
+        } else abrirArchivo();
+
+    }//GEN-LAST:event_bArchivoAbrirActionPerformed
+
+    private void bArchivoNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bArchivoNuevoActionPerformed
+        if (modificado){
+            int opcion = popupCambios();
+            switch (opcion){
+                case 0 -> {
+                    // Abrir ventana para elegir donde guardar el archivo
+                    int i = guardarArchivoComo();
+                    if (i == 0){
+                        nuevoArchivo();
+                    }
+                }
+                case 1 -> {
+                    // Limpiar el editor de texto sin guardar nada
+                    nuevoArchivo();
+                }
+            }
+        } else {
+            nuevoArchivo();
+        }
+    }//GEN-LAST:event_bArchivoNuevoActionPerformed
 
     private void nuevoArchivo() {
         panelTexto.setText("");
@@ -241,103 +361,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
         }
         return 1; // No hay cambios, se cierra
     }
-    
-    private void bArchivoAbrirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bArchivoAbrirActionPerformed
-        if (modificado){
-            int opcion = popupCambios();
-            switch (opcion){
-                case 0 -> {
-                    // Abrir ventana para elegir donde guardar el archivo y abrir el archivo
-                    int i = guardarArchivoComo();
-                    if (i == 0) {
-                        // Si no se cancela el guardado del archivo, se abre el archivo deseado
-                        abrirArchivo();
-                    }
-                }
-                case 1 -> {
-                    // Abre el archivo sin guardar lo anterior
-                    abrirArchivo();
-                }
-                case 2, -1 -> {
-                    // No hacer nada
-                }
-            }
-        } else abrirArchivo();
- 
-    }//GEN-LAST:event_bArchivoAbrirActionPerformed
-
-    private void bArchivoGuardarComoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bArchivoGuardarComoActionPerformed
-        guardarArchivoComo();
-    }//GEN-LAST:event_bArchivoGuardarComoActionPerformed
-
-    private void bArchivoNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bArchivoNuevoActionPerformed
-        if (modificado){
-            int opcion = popupCambios();
-            switch (opcion){
-                case 0 -> {
-                    // Abrir ventana para elegir donde guardar el archivo
-                    int i = guardarArchivoComo();
-                    if (i == 0){
-                        nuevoArchivo();
-                    }
-                }
-                case 1 -> {
-                    // Limpiar el editor de texto sin guardar nada
-                    nuevoArchivo();
-                }
-            }
-        } else {
-            nuevoArchivo();
-        }
-    }//GEN-LAST:event_bArchivoNuevoActionPerformed
-
-    private void botonArchivoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonArchivoActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_botonArchivoActionPerformed
-    
-    private void bArchivoSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bArchivoSalirActionPerformed
-        int cerrar = cerrarPrograma();
-        if (cerrar == 1){
-            dispose();
-        }
-    }//GEN-LAST:event_bArchivoSalirActionPerformed
-
-    private void bArchivoGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bArchivoGuardarActionPerformed
-        if (archivoActual.getSelectedFile() == null){
-            guardarArchivoComo();
-        } else {
-            PrintWriter buffer;
-            try {
-                buffer = new PrintWriter(archivoActual.getSelectedFile());
-                buffer.print(panelTexto.getText());
-                buffer.close();
-                tituloVentana = archivoActual.getSelectedFile().getName() + " - Analizador Léxico";
-                setTitle(tituloVentana);
-                modificado = false;
-            } catch (FileNotFoundException ex) {
-                Logger.getLogger(VentanaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }//GEN-LAST:event_bArchivoGuardarActionPerformed
-
-    private void bAnalizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bAnalizarActionPerformed
-        if (archivoActual.getSelectedFile() == null){
-            int guardado = guardarArchivoComo();
-            if (guardado == 1){
-                return;
-            }
-        }
-        try {
-            FileReader lectorArchivo = new FileReader(archivoActual.getSelectedFile().getPath());
-            Lexico lexer = new Lexico(lectorArchivo);
-            lexer.next_token();
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(VentanaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(VentanaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }//GEN-LAST:event_bAnalizarActionPerformed
-
+        
 
     public static void main(String args[]) {
         
@@ -360,6 +384,8 @@ public class VentanaPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenu botonArchivo;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JPopupMenu.Separator jSeparator1;
+    private javax.swing.JToolBar jToolBar1;
+    private javax.swing.JLabel lnCol;
     private javax.swing.JEditorPane panelTexto;
     // End of variables declaration//GEN-END:variables
 }
