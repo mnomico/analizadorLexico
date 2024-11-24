15d1e65a4527c77499d4689b339e15f797379bd3
mnomico <nomicomateou@gmail.com>
2024-11-24 19:26:42 -0300

    change f file path from local path to repository path

--- src/main/java/analizadorLexico/Main.java
+++ src/main/java/analizadorLexico/Main.java
@@ -6,7 +6,7 @@ import java.io.FileReader;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException, Exception {
         // VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
-        FileReader f = new FileReader("C:\\Users\\Tomas Resnik\\Documents\\Unlu\\Teoria de la Computacion\\analizadorLexico\\src\\main\\java\\analizadorLexico\\pruebense.txt");
+        FileReader f = new FileReader("src/main/java/analizadorLexico/pruebense.txt");
         Lexico Lexer = new Lexico(f);
         parser sintactico = new parser(Lexer);
         sintactico.parse();
